dd095d3493f15a93aac7af15fe9804489c2eae44
JohnnHidalgo <johnna.7h@gmail.com>
2020-05-24 15:55:34 -0400

    Create List User Component

---
+++ src/app/User/list-user/list-user.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from '../../Service/service.service'
+import { User } from 'src/app/Modelo/User';
+
+@Component({
+  selector: 'app-list-user',
+  templateUrl: './list-user.component.html',
+  styleUrls: ['./list-user.component.css']
+})
+export class ListUserComponent implements OnInit {
+
+  user: User[];
+  constructor(private service: ServiceService, private router: Router) { }
+
+  ngOnInit() {
+    this.service.getUser()
+      .subscribe(data => {
+        this.user = data;
+      });
+  }
+  editUser(user:User):void{
+    localStorage.setItem("id",user.idUser.toString());
+    this.router.navigate(["edit"]);
+  }
+
+  Delete(user:User){
+    this.service.deleteUser(user)
+    .subscribe(data=>{
+      this.user=this.user.filter(p=>p!==user);
+      alert("Usuario eliminado...");
+    })
+  }
+
+
+}
