c6de834641304cc358d208f419231a154d5e90f1
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2020-06-10 16:39:56 -0400

    Map Test

--- src/app/view/dashboard/dashboard.component.ts
+++ src/app/view/dashboard/dashboard.component.ts
@@ -1,25 +1,22 @@
-import { AfterViewInit, Component, ViewChild, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ViewChild, OnInit, TemplateRef } from '@angular/core';
 import { IgxGeographicMapComponent } from 'igniteui-angular-maps';
+import { IgxShapeDataSource } from 'igniteui-angular-core';
+import { IgxGeographicHighDensityScatterSeriesComponent } from 'igniteui-angular-maps';
+import WorldUtils from './WorldUtils';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  @ViewChild ("map")
+export class DashboardComponent implements OnInit, AfterViewInit {
+  constructor() {}
+  @ViewChild ('map')
   public map: IgxGeographicMapComponent;
-  
-    constructor() {}
+  @ViewChild('template')
+  public tooltip: TemplateRef<object>;
+  public geoLocations;
 
-  public ngAfterViewInit(): void {
-    this.map.windowRect = { left: 0.2, top: 0.1, width: 0.7, height: 0.7 };
-  }
-
-  ngOnInit() {
-  }
-
-  
   initOpts = {
     renderer: 'svg',
     width: 400,
@@ -56,9 +53,44 @@ export class DashboardComponent implements OnInit {
       name: 'Counters',
       type: 'bar',
       barWidth: '60%',
-      data: [10, 52, 200, 334, 390, 330, 220]
+      data: [15, 52, 200, 334, 390, 330, 220]
     }]
   };
+
+  ngOnInit() {
+  }
+
+  public ngAfterViewInit(): void {
+    // fetching geographic locations from public JSON folder
+    fetch('assets/Data/AusPlaces.json')
+      .then((response) => response.json())
+      .then((data) => this.onDataLoaded(data, ''));
+  }
+
+  public onDataLoaded(sds: IgxShapeDataSource, e: any) {
+    this.geoLocations = sds;
+    // creating HD series with loaded data
+    const geoSeries = new IgxGeographicHighDensityScatterSeriesComponent();
+    geoSeries.dataSource = sds;
+    geoSeries.longitudeMemberPath = 'x';
+    geoSeries.latitudeMemberPath = 'y';
+    geoSeries.heatMaximumColor = 'Red';
+    geoSeries.heatMinimumColor = 'Black';
+    geoSeries.heatMinimum = 0;
+    geoSeries.heatMaximum = 5;
+    geoSeries.pointExtent = 1;
+    geoSeries.tooltipTemplate = this.tooltip;
+    geoSeries.mouseOverEnabled = true;
+
+    // adding HD series to the geographic amp
+    this.map.series.add(geoSeries);
+
+    // zooming to bound of all geographic locations
+    const geoBounds = WorldUtils.getBounds(this.geoLocations);
+    geoBounds.top = 0;
+    geoBounds.height = -50;
+    this.map.zoomToGeographic(geoBounds);
+  }
 
 
 
