e84d9acbbf853cab82d3b790ceaf3f0d17f46eca
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2020-06-11 21:24:28 -0400

    Added Service Connections for all Modules

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../model/User';
 import { Dashboard } from '../model/Dashboard';
 import { View } from '../model/View';
+import { Attribute } from '../model/Attribute';
+import { Graphic } from '../model/Graphic';
+import { GraphicType } from '../model/GraphicType';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +18,11 @@ export class ServiceService {
   Url='http://localhost:8080/ejemplo01/k1/user/';
   dashboardUrl='http://localhost:8080/ejemplo01/dashboard';
   viewUrl='http://localhost:8080/ejemplo01/k1/vista/';
+  attributeUrl='http://localhost:8080/ejemplo01/k1/atributo/';
+  graphicUrl='http://localhost:8080/ejemplo01/k1/grafico/';
+  graphicTypeUrl='http://localhost:8080/ejemplo01/k1/tipo_grafico/';
+
+
 
   getUser(){  
     return this.http.get<User[]>(this.Url);
@@ -34,7 +42,6 @@ export class ServiceService {
     //borrado fisico
     //return this.http.delete<User>(this.Url+"/"+user.id);
   }
-
   loginUser(user:User){
     return this.http.put<User>(this.Url+"/"+user.idUser,user);
   }
@@ -59,14 +66,88 @@ export class ServiceService {
     //return this.http.delete<User>(this.Url+"/"+user.id);
   }
 
+
   getView(){
     return this.http.get<View[]>(this.viewUrl);
-    
     //return this.http.get<View[]>(this.viewUrl);
   }
-
+  createView(view:View){
+    return this.http.post<View[]>(this.viewUrl,view);
+  }
   getViewId(id:number){
     return this.http.get<View>(this.viewUrl+"/"+id);
   }
+  updateView(view:View){
+    return this.http.put<View>(this.viewUrl+"/"+view.idView,view);
+  }
+  deleteView(view:View){
+    //borrado logico
+    return this.http.put<View>(this.viewUrl+"/"+view.idView,view);
+    //borrado fisico
+    //return this.http.delete<User>(this.Url+"/"+user.id);
+  }
+
+
+  getAttribute(){
+    return this.http.get<Attribute[]>(this.attributeUrl);
+    //return this.http.get<View[]>(this.viewUrl);
+  }
+  createAttribute(attribute:Attribute){
+    return this.http.post<Attribute[]>(this.attributeUrl,attribute);
+  }
+  getAttributeId(id:number){
+    return this.http.get<Attribute>(this.attributeUrl+"/"+id);
+  }
+  updateAttribute(attribute:Attribute){
+    return this.http.put<Attribute>(this.attributeUrl+"/"+attribute.idAttribute,attribute);
+  }
+  deleteAttribute(attribute:Attribute){
+    //borrado logico
+    return this.http.put<Attribute>(this.attributeUrl+"/"+attribute.idAttribute,attribute);
+    //borrado fisico
+    //return this.http.delete<User>(this.Url+"/"+user.id);
+  }
+
+
+  getGraphic(){
+    return this.http.get<Graphic[]>(this.graphicUrl);
+    //return this.http.get<View[]>(this.viewUrl);
+  }
+  createGraphic(graphic:Graphic){
+    return this.http.post<Graphic[]>(this.graphicUrl,graphic);
+  }
+  getGraphicId(id:number){
+    return this.http.get<Graphic>(this.graphicUrl+"/"+id);
+  }
+  updateGraphic(graphic:Graphic){
+    return this.http.put<Graphic>(this.graphicUrl+"/"+graphic.idGraphic,graphic);
+  }
+  deleteGraphic(graphic:Graphic){
+    //borrado logico
+    return this.http.put<Graphic>(this.graphicUrl+"/"+graphic.idGraphic,graphic);
+    //borrado fisico
+    //return this.http.delete<User>(this.Url+"/"+user.id);
+  }
+
+  getGraphicType(){
+    return this.http.get<GraphicType[]>(this.graphicTypeUrl);
+    //return this.http.get<View[]>(this.viewUrl);
+  }
+  createGraphicType(graphicType:GraphicType){
+    return this.http.post<GraphicType[]>(this.graphicTypeUrl,graphicType);
+  }
+  getGraphicTypeId(id:number){
+    return this.http.get<GraphicType>(this.graphicTypeUrl+"/"+id);
+  }
+  updateGraphicType(graphicType:GraphicType){
+    return this.http.put<GraphicType>(this.graphicTypeUrl+"/"+graphicType.idGraphicType,graphicType);
+  }
+  deleteGraphicType(graphicType:GraphicType){
+    //borrado logico
+    return this.http.put<GraphicType>(this.graphicTypeUrl+"/"+graphicType.idGraphicType,graphicType);
+    //borrado fisico
+    //return this.http.delete<User>(this.Url+"/"+user.id);
+  }
+
 
 }
